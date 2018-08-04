e9b73a54982ffe6b6e90193bc75457ad4750b3f4
Thomas Schmidt <ithommy@me.com>
2018-08-04 22:31:29 +0200

    Feedback from reviewer implemented Changes for NoEmptyJokesTest Added auto gradle task (Start--> Run tests --> Stops the Server)

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/NoEmptyJokesTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/NoEmptyJokesTest.java
@@ -6,6 +6,9 @@ import android.test.AndroidTestCase;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
 /**
  * BuildItBigger
  * Created by Thomas Schmidt on 04.08.2018.
@@ -15,15 +18,19 @@ import org.junit.runner.RunWith;
 public class NoEmptyJokesTest extends AndroidTestCase {
 
     @Test
-    public void testForNonEmptyJoke(){
+    public void testForNonEmptyJoke() throws InterruptedException {
+
+        final CountDownLatch latch = new CountDownLatch(1);
 
         JokeAsyncTask jokeAsyncTask = new JokeAsyncTask(new JokeAsyncTask.JokeLoadingListener() {
             @Override
             public void jokeLoaded(String joke) {
                 assertNotNull(joke);
+                assertTrue(joke.length() > 0);
+                latch.countDown();
             }
         });
-
         jokeAsyncTask.execute();
+        latch.await(10, TimeUnit.SECONDS);
     }
 }
